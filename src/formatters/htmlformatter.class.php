edbeee11895cc13be63fb3753ad642435d51f6bb
Mark Watkinson <markwatkinson@gmail.com>
2011-04-26 12:32:46 +0100

    Neaten HTML formatter, sort out coding standards

--- src/formatters/htmlformatter.class.php
+++ src/formatters/htmlformatter.class.php
@@ -1,97 +1,91 @@
 <?php
 /// @cond ALL
-class LuminousFormatterHTML extends LuminousFormatter
-{   
+class LuminousFormatterHTML extends LuminousFormatter {
+
   public $height = 0;
   /** 
    * strict HTML standards: the target attribute won't be used in links
    * \since  0.5.7
    */
   public $strict_standards = false;
-  
-  protected $numberless_template = '<div class="code"><pre class="code">%s</pre></div>';
-  
-  protected $numbered_template = '<table class="code_container"><tr><td class="line_number_bar"><pre class="line_numbers">%s</pre></td><td class="code"><pre class="code">%s</pre></td></tr></table>';
 
-  protected $template = '<div class="luminous"><div class="code_container" %s>%s</div></div>';
+  /// line number-less
+  protected $numberless_template = '<div class="code">
+  <pre class="code">%s</pre>
+</div>';
 
-  private function Format_NoLineNumbers($src)
-  {
+  /// line numbered
+  protected $numbered_template = '<table class="code_container">
+  <tr>
+    <td class="line_number_bar">
+      <pre class="line_numbers">%s</pre>
+    </td>
+    <td class="code">
+      <pre class="code">%s</pre>
+    </td>
+  </tr>
+</table>';
+
+  /// container template, placeholders are 1: inline style 2: code
+  protected $template = '<div class="luminous">
+  <div class="code_container" %s>%s</div>
+</div>';
+
+  
+  private function format_numberless($src) {
     $lines = array();
-    $lines_a = explode("\n", $src);
-    $num = count($lines_a);
-    foreach($lines_a as $index=>$line)
-    {
+    $lines_original = explode("\n", $src);
+    foreach($lines_original as $line) {
       $l = $line;
       $num = $this->WrapLine($l, $this->wrap_length);
       // strip the newline if we're going to join it. Seems the easiest way to 
-      // fix issue 10. http://code.google.com/p/luminous/issues/detail?id=10
+      // fix http://code.google.com/p/luminous/issues/detail?id=10
       $l = substr($l, 0, -1);
-      $lines []= $l;
+      $lines[] = $l;
     }
     $lines = implode("\n", $lines);
-    return sprintf($this->numberless_template, $lines);    
-  } 
+    return sprintf($this->numberless_template, $lines);
+  }
   
   
-  public 
-  function format($src)
-  {
+  public function format($src) {
     $line_numbers = false;
-/*   
-    if ($this->tab_width - 1 > 0)
-    {
-      $tab_rep = "";
-      for($i=0; $i<$this->tab_width; $i++)
-        $tab_rep .= " ";
-      $src = str_replace("\t", $tab_rep, $src);
-    }
-*/
-    if ($this->link)
-      $src = $this->Linkify($src);
-    
-    $lines = "";
-    if ($this->line_numbers)
-      $lines = $this->Format_LineNumbers($src);
-    else
-      $lines = $this->Format_NoLineNumbers($src);
 
+    if ($this->link)  $src = $this->linkify($src);
     
-    $lines = preg_replace('/(?<=<\/)[A-Z_0-9]+(?=>)/', 'span', $lines);
-    
+    $code_block = $this->line_numbers? $this->format_numbered($src)
+      : $this->format_numberless($src);
+
+    // convert </ABC> to </span>
+    $code_block = preg_replace('/(?<=<\/)[A-Z_0-9]+(?=>)/S', 'span',
+      $code_block);
+    // convert <ABC> to <span class=ABC>
     $cb = create_function('$matches', 
                           '$m1 = strtolower($matches[1]);
                           return "<span class=\'" . $m1 . "\'>";
                           ');
+    $code_block = preg_replace_callback('/<([A-Z_0-9]+)>/', $cb, $code_block);
     
-    $lines = preg_replace_callback('/<([A-Z_0-9]+)>/', $cb, 
-                                   $lines);
-    $markup =  $lines ; 
-    $h = "" . $this->height;
-    $h = trim($h);
-    $css = "";
+    $height = trim('' . $this->height);
+    $css = '';
     
-    if (strlen($h) && (int)$h > 0)
-    {
-      $units = !ctype_digit($h[strlen($h)-1]);
-      $css = " style=\"max-height:";
-      $css .= $h;
-      if (!$units)
-        $css .= "px";
-      $css .= ";\" ";
+    if (!empty($height) && (int)$height > 0) {
+      // look for units, use px is there are none
+      if (!preg_match('/\D$/', $height)) $height .= 'px';
+      $css = " style='max-height: {$height};'";
     }
     else 
-      $css = ' style="overflow:visible !important"';
-    
-    return sprintf($this->template, $css, $markup);
+      $css = ' style="overflow:visible !important;"';
+    return sprintf($this->template, $css, $code_block);
   }
   
-  
-  protected function Linkify_cb($matches)
-  {
-    $uri = (isset($matches[1]) && strlen(trim($matches[1])))? $matches[0] : 
-    "http://" . $matches[0];
-    
+  /**
+   * Detects and links URLs - callback
+   */
+  protected function linkify_cb($matches) {
+    $uri = (isset($matches[1]) && strlen(trim($matches[1])))? $matches[0]
+      : "http://" . $matches[0];
+
     // we dont want to link if it would cause malformed HTML
     $open_tags = array();
     $close_tags = array();
@@ -109,13 +103,14 @@ class LuminousFormatterHTML extends LuminousFormatter
     
     $uri = strip_tags($uri);
     
-    $target = ($this->strict_standards)? '' : 'target="_blank"';
-    return "<a href=\"$uri\" class=\"link\" $target>" 
-      . "$matches[0]</a>";
+    $target = ($this->strict_standards)? '' : ' target="_blank"';
+    return "<a href='{$uri}' class='link'{$target}>{$matches[0]}</a>";
   }
   
-  protected function Linkify($src)
-  {    
+  /**
+   * Detects and links URLs
+   */
+  protected function linkify($src) {
     if (stripos($src, "http") === false && stripos($src, "www") === false)
         return $src;
     
@@ -132,7 +127,7 @@ class LuminousFormatterHTML extends LuminousFormatter
       # line-enclosing tags.
       (?:[/$chars/?=\#;]+|&amp;|<[^>]+>(?!$))*
       @xm",
-      array($this, 'Linkify_cb'), $src);
+      array($this, 'linkify_cb'), $src);
     // this can hit a backtracking limit, in which case it nulls our string
     // FIXME: see if we can make the above regex more resiliant wrt
     // backtracking
@@ -142,19 +137,17 @@ class LuminousFormatterHTML extends LuminousFormatter
     return $src;
   }
   
-  // this is what we're using now for line numbering.
-  private 
-  function Format_LineNumbers($src)
-  {    
+  
+  private function format_numbered($src) {
     
     $lineno = 1;
-    $linenos='';
+    $linenos = '';
     $lines = '';
     
     $lines_a = explode("\n", $src);
     $lines_untagged = explode("\n", strip_tags($lines));
     
-    $id = rand();    
+    $id = rand();
     
     // this seems to run a bit faster if we keep the literals out of
     // the loop.
@@ -174,46 +167,43 @@ class LuminousFormatterHTML extends LuminousFormatter
     $class_alt = ' line_alt';
     $line_tag2 = '">';
     $line_tag3 = '</span>';
-    
+
     $line_delta = 3;
-    foreach($lines_a as $line)
-    {      
-      
+    foreach($lines_a as $line)  {
+
       $linenos .= $line_no_tag0 . $lineno . $line_no_tag1;
       if ($lineno % 5 === 0)
         $linenos .= $class_emph;
       $linenos .= $line_no_tag2 . $lineno . $line_no_tag3;
-      
-      
-      $num = $this->WrapLine($line, $this->wrap_length);  
-      
+
+      $num = $this->WrapLine($line, $this->wrap_length);
+
       for ($i=1; $i<$num; $i++)
         $linenos .= $wrap_line;
-      
-      
+
       $lines .= $line_tag0 . $lineno . $line_tag1;
       if ($lineno % 2 === 0)
         $lines .= $class_alt;
       $lines .= $line_tag2 . $line . $line_tag3;
-      
+
       ++$lineno;
-      
+
     }
     return sprintf($this->numbered_template, $linenos, $lines);
-  }      
+  }
 }
 
 
-class LuminousFormatterHTMLInline extends LuminousFormatterHTML
-{   
-  protected $template =  '<div class="luminous luminous_inline"><div class="code_container" %s>%s</div></div>';
-    
-  public function format($src) 
-  {
+class LuminousFormatterHTMLInline extends LuminousFormatterHTML {
+  protected $template = '<div class="luminous luminous_inline">
+  <div class="code_container" %s>%s</div>
+</div>';
+
+  public function format($src) {
     $this->line_numbers = false;
     $this->height = 0;
     return parent::format($src);
   }
-  
+
 }
 /// @endcond
