6988ba17acd4bdd2b8059e166329f8e304ca065c
Mark Watkinson <markwatkinson@gmail.com>
2011-04-29 16:54:43 +0100

    add version number to readme in packaging script

--- package.py
+++ package.py
@@ -29,26 +29,37 @@ def print_help():
   Version should correspond to a tag in the Git repository. If it is omitted, 
   'master' is used.'''.format(sys.argv[0]))
 
-def do_production():
+def do_production(data):
   """ unset the debug flag in the PHP source """
   subprocess.call(['sed', '-i', 
     "s/define('LUMINOUS_DEBUG', true);/define('LUMINOUS_DEBUG', false);/",
     "src/debug.php"])
 
 
-def do_removals():
+def do_removals(data):
   for d in to_remove: 
     if os.path.isdir(d): shutil.rmtree(d)
     elif os.path.isfile(d): os.remove(d)
     
-def do_doxygen():
+def do_doxygen(data):
   try:
     subprocess.call(['doxygen'])
   except OSError as e:
     print('Warning: failed to execute doxygen: ' + str(e))
-  
 
-functions = [do_removals, do_doxygen, do_production]
+def do_version(data): 
+  version = data
+  f = open('README.markdown')
+  s = f.read().splitlines()
+  s[0] += ' - ' + version
+  f.close()
+  f = open('README.markdown', 'w')
+  f.write('\n'.join(s))
+  f.close()
+  
+    
+
+functions = [do_removals, do_doxygen, do_production, do_version]
 
 def package(version):
   # move into the dist dir
@@ -82,7 +93,7 @@ def package(version):
   os.rename(dirname_, dirname)
   # move into the root dir and execute the functions
   os.chdir(dirname)
-  for f in functions: f()
+  for f in functions: f(version)
 
   
   # now move back out of it and tar/zip it up
