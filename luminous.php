a81782473d547dcc94f9cb24082e79de635f81a6
Mark Watkinson <markwatkinson@gmail.com>
2011-05-13 14:37:53 +0100

    Add command line interface

--- luminous.php
+++ luminous.php
@@ -1,5 +1,15 @@
 <?php
 
-/* dummy file, includes src/luminous.php */
+/*
+ * If we're running in a web environment, this is simply an include
+ * file which includes everythign necessary to use Luminous.
+ *
+ * If we're running in CLI-mode then this handles the CLI interface.
+ */
 
 require_once(dirname(__FILE__) . '/src/luminous.php');
+
+if (PHP_SAPI === 'cli') {
+  require(dirname(__FILE__) . '/src/cli.php');
+  // cli mode
+}
