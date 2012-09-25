7957a5f0e3bdf8819c0ea4d0648434261314371a
Mark Watkinson <markwatkinson@gmail.com>
2012-09-25 14:30:32 +0100

    Add a top level example page + options example

---
+++ examples/options.php
@@ -0,0 +1,33 @@
+<?php
+include dirname(__FILE__) . '/helper.inc';
+$code = <<<EOF
+// here's some jquery - http://www.jquery.com
+$('a').click(function() {
+    return false;
+});
+EOF;
+$language = 'js';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Setting options</title>
+        <?= luminous::head_html() ?>
+    </head>
+    
+    <body>
+        <p> There are two ways you can set options: globally via the set() method, and per-call
+          in the highlight() method. Let's disable auto-linking and make all
+          highlights begin at line 17 via the global call.
+        <?php
+          // NOTE: this is equivalent to calling luminous::set(array('auto-link' => false, 'start-line' => 17));
+          luminous::set('auto-link', false);
+          luminous::set('start-line', 17);
+          echo luminous::highlight($language, $code);
+       ?>
+       <p> Now let's override both of those for the duration of the next call
+       <?= luminous::highlight($language, $code, array('auto-link' => true, 'start-line' => 1)) ?>
+       <p> When we next call highlight(), the options will be back to their global states:
+       <?= luminous::highlight($language, $code); ?>
+    </body>
+</html>
